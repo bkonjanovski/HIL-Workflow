ff90e360931dc6ad9ee7198119af1d8a15dab44f
bkonjanovski-novelic <borjan.konjanovski@novelic.com>
2023-12-07 16:49:28 +0100

    Changed baurate to 9600

--- src/uart.py
+++ src/uart.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
  
 def echo_serial(input: bytes):
         if not isinstance(input, (bytes)) or len(input) != 1:  
