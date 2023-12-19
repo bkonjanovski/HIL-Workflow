dccad26e0039ab277d6dce5da807457a0a73b762
bkonjanovski-novelic <borjan.konjanovski@novelic.com>
2023-12-19 16:21:43 +0100

    Modified tests

--- src/uart.py
+++ src/uart.py
@@ -1,6 +1,12 @@
 import serial
+import time
+import RPi.GPIO as GPIO
+
 
 ser = serial.Serial('/dev/ttyACM0', 115200)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
  
 def echo_serial(input: bytes):
         if not isinstance(input, (bytes)) or len(input) != 1:  
@@ -8,3 +14,10 @@ def echo_serial(input: bytes):
         ser.write(input)
         res = ser.read(1)
         return res.decode()
+
+
+def check_gpio_signal(): 
+	GPIO.output(17, GPIO.HIGH)
+	res = ser.read()
+        return res.decode()
+
--- tests/test_uart.py
+++ tests/test_uart.py
@@ -1,20 +1,30 @@
 import pytest  
-from src.uart import echo_serial
+from src.uart import check_gpio_signal
 import time
 
-def test_echo_single_byte():  
+check_gpio_signal
+
+def test_gpio_sgnal():  
     time.sleep(3)
-    assert echo_serial("1".encode()) == '1'  
+    assert check_gpio_signal() == 'TEST-TX'
 
-def test_echo_multiple_bytes():  
-    with pytest.raises(TypeError):  
-        echo_serial(b'123')  
+#def test_echo_single_byte():  
+#    time.sleep(3)
+#    assert echo_serial("1".encode()) == '1'  
 
-def test_echo_number():  
-    with pytest.raises(TypeError):  
-        echo_serial(123)         
+#def test_echo_single_byte():  
+#    time.sleep(3)
+#    assert echo_serial("1".encode()) == '1'  
+
+#def test_echo_multiple_bytes():  
+#    with pytest.raises(TypeError):  
+#        echo_serial(b'123')  
+
+#def test_echo_number():  
+#    with pytest.raises(TypeError):  
+#        echo_serial(123)         
   
-def test_echo_string():  
-    with pytest.raises(TypeError):  
-        echo_serial("123")  
+#def test_echo_string():  
+#    with pytest.raises(TypeError):  
+#        echo_serial("123")  
   
